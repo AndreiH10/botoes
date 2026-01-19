4afcd8b5acd64d1c1086c954dd181606a36a887b
AndreiH10 <andreihenrique1607@gmail.com>
2026-01-19 18:07:02 -0300

    mudarStatus

--- script.js
+++ script.js
@@ -7,13 +7,39 @@ function botaoAtivado(botao){
     }
 }
 
-const botoes = document.querySelectorAll('.btn');
+ async function mudarStatus(novoEstado) {
 
-    botoes.forEach(botao => {
-        botao.addEventListener('click', function() {
-            // toggle: se não tem a classe 'clicado', ele coloca. Se já tem, ele tira.
-            this.classList.toggle('clicado');
-        });
+    await fetch('http://127.0.0.1:8000/set-status', {
+
+        method: 'POST', // O método para "setar" informações
+
+        headers: {
+
+            'Content-Type': 'application/json'
+
+        },
+
+        body: JSON.stringify({ ligado: novoEstado }) // Transforma o objeto JS em JSON
+
     });
 
-    
+}
+
+const botoes = document.querySelectorAll('.btn');
+
+botoes.forEach(botao => {
+    botao.addEventListener('click', function() {
+        // 1. Alterna a classe visual
+        this.classList.toggle('clicado');
+
+        // 2. Verifica se está clicado e envia o status
+        const estaAtivo = this.classList.contains('clicado');
+        mudarStatus(estaAtivo);
+        
+        if(estaAtivo) {
+            alert("Ligado");
+        } else {
+            alert("Desligado");
+        }
+    });
+});
